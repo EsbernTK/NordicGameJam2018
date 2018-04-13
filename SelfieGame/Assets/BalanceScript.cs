ecb6f07909f6798220e47ae3b400f7d86ab2bbee
EsbernTK <esberntk@gmail.com>
2018-04-14 00:36:06 +0200

    Added Simple Center of mass calculations

---
+++ SelfieGame/Assets/BalanceScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalanceScript : MonoBehaviour {
+    public List<BodyPart> bodyParts;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public Vector3 CalcCenterOfMass()
+    {
+        Vector3 center = Vector3.zero;
+        float totWeight = 0;
+        foreach(BodyPart part in bodyParts)
+        {
+            center += part.transform.position * part.weight;
+            totWeight += part.weight;
+        }
+
+        return center/totWeight;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(CalcCenterOfMass(), 0.1f);
+    }
+}
