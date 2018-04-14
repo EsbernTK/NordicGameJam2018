5691d8379facacab26c955fb60ddf25120139129
Nikolaj Støvring <nstovring@live.dk>
2018-04-14 11:00:10 +0200

    Selfie Evaluator

---
+++ SelfieGame/Assets/SelfieEvaluator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfieEvaluator : MonoBehaviour {
+
+    // Use this for initialization
+    public List<Transform> importantFeatures;
+    public Camera cam;
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TakeSelfie()
+    {
+
+    }
+    int succesfullPoints = 0;
+    void EvaluateSelfie()
+    {
+        
+    }
+
+}
