1e2a09fb73c478248f15c38a4b8013f33dc11c77
Nikolaj Støvring <nstovring@live.dk>
2018-04-14 16:46:22 +0200

    AudioManager, Bird and train tracks

---
+++ SelfieGame/Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    [System.Serializable]
+    public class AudioEffect{
+        public string effectName;
+        public AudioSource a_Source;
+        public AudioClip a_clip;
+        public bool playing;
+
+        public void PlayEffect()
+        {
+            a_Source.PlayOneShot(a_clip);
+        }
+    }
+
+    public List<AudioEffect> audioEffects;
+}
